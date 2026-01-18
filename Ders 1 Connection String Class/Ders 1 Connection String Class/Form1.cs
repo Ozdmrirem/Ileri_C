d562d05d9f0f18182e13ea47c86f021c2e3e01ed
irem <146566414+Ozdmrirem@users.noreply.github.com>
2026-01-18 23:27:56 +0300

    Add files via upload

---
+++ Ders 1 Connection String Class/Ders 1 Connection String Class/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Ders_1_Connection_String_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sqlbaglantisi bgl = new Sqlbaglantisi();
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Books",bgl.baglanti());
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
---
+++ Ders 1 Connection String Class/Ders 1 Connection String Class/Sqlbaglantisi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Ders_1_Connection_String_Class
+{
+    class Sqlbaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=DESKTOP-HJ161GJ\SQLEXPRESS;Initial Catalog=DbLibrary;Integrated Security=True;Encrypt=False");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
