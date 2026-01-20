e9ccdca4ca32c58cb7c35efc6c285a4479b533c8
irem <146566414+Ozdmrirem@users.noreply.github.com>
2026-01-20 23:40:25 +0300

    Add files via upload

--- Ders 3/Ders 3/Program.cs
+++ Ders 3/Ders 3/Program.cs
@@ -17,6 +17,7 @@ namespace Ders_3
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //
         }
     }
 }
