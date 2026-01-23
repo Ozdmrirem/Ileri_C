11f5d5dda5eaae5c736bbba890d53e060237a893
irem <146566414+Ozdmrirem@users.noreply.github.com>
2026-01-23 23:46:44 +0300

    Add files via upload

---
+++ Ders 8/Ders 8/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Ders_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string baglantiadresi;
+            StreamReader oku = new StreamReader("E:\\Adres.txt");
+            string satir = oku.ReadLine();
+            while ( satir != null ) 
+                {
+                     
+                }
+        }
+    }
+}
