16c8adb10d3a1fe27f434ddaf3f395e11983287f
irem <146566414+Ozdmrirem@users.noreply.github.com>
2026-01-26 16:27:17 +0300

    Add files via upload

---
+++ Ders 10/Ders 10/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int toplam(int s1,int s2)
+        {
+            int s3= s1 + s2;
+            return s3;
+        }
+
+        private int kupbul(int x)
+        {
+            int sonuc = x * x * x;
+            return sonuc;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = toplam(5, 10).ToString();
+            label2.Text = toplam(15, 25).ToString();
+            label3.Text = toplam(50, 75).ToString();
+
+            int sayi = Convert.ToInt16(textBox1.Text);
+            label4.Text = kupbul(sayi).ToString();
+        }
+    }
+}
+
+
+
+
