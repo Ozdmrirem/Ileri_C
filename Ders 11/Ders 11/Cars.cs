086e472300a98c9bd1f98b7bbf7546142d9dd3fc
irem <146566414+Ozdmrirem@users.noreply.github.com>
2026-01-30 23:58:40 +0300

    Add files via upload

---
+++ Ders 11/Ders 11/Cars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_11
+{
+    internal class Cars
+    {
+        string renk;
+        string fiyat;
+        int hiz;
+        char durum;
+        int model;
+    }
+}
